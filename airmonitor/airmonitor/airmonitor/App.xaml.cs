bd5fdc767a6666270f9e4d645f76ee4dc73e3b3a
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-05-31 21:39:20 +0200

    Add config to resources

--- airmonitor/airmonitor/airmonitor/App.xaml.cs
+++ airmonitor/airmonitor/airmonitor/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using airmonitor.Views;
-using System;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,11 +12,44 @@ namespace airmonitor
 {
     public partial class App : Application
     {
+        public static string AirlyApiKey { get; private set; }
+        public static string AirlyApiUrl { get; private set; }
+        public static string AirlyApiMeasurementUrl { get; private set; }
+        public static string AirlyApiInstallationUrl { get; private set; }
+
         public App()
         {
             InitializeComponent();
 
+            InitializeApp();
+        }
+
+        private void InitializeApp()
+        {
+            LoadConfig();
+
             MainPage = new RootTabbedPage();
+        }
+
+        private static void LoadConfig()
+        {
+            var assembly = Assembly.GetAssembly(typeof(App));
+            var resourceNames = assembly.GetManifestResourceNames();
+            var configName = resourceNames.FirstOrDefault(s => s.Contains("config.json"));
+
+            using (var stream = assembly.GetManifestResourceStream(configName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var json = reader.ReadToEnd();
+                    var dynamicJson = JObject.Parse(json);
+
+                    AirlyApiKey = dynamicJson["AirlyApiKey"].Value<string>();
+                    AirlyApiUrl = dynamicJson["AirlyApiUrl"].Value<string>();
+                    AirlyApiMeasurementUrl = dynamicJson["AirlyApiMeasurementUrl"].Value<string>();
+                    AirlyApiInstallationUrl = dynamicJson["AirlyApiInstallationUrl"].Value<string>();
+                }
+            }
         }
 
         protected override void OnStart()
